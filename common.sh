516864148f19ca52efd377368fc74191a550ddde
chr0n1x <heilong24@gmail.com>
2013-08-30 17:01:55 -0400

    Removing stuff from home.html

--- common.sh
+++ common.sh
@@ -4,7 +4,7 @@ export PYTHONPATH=.:./dependencies/lib:$PYTHONPATH
 export CAPTURE_SCRIPT=dpxdt/client/capture.js
 
 # Update these for your environment:
-export PHANTOMJS_BINARY=phantomjs
+export PHANTOMJS_BINARY=./phantomjs
 
 # change this accordingly
 export DPXDT_HOSTNAME=localhost
@@ -14,4 +14,4 @@ export RELEASE_SERVER_PREFIX=http://${DPXDT_HOSTNAME}:5000/api
 export QUEUE_SERVER_PREFIX=http://${DPXDT_HOSTNAME}:5000/api/work_queue
 
 # Update this for your deployment environment:
-export PHANTOMJS_DEPLOY_BINARY=phantomjs
+export PHANTOMJS_DEPLOY_BINARY=./phantomjs
